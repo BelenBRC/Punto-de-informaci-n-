d6876c290579c4a0a0b22e3c465f233814182947
BelenBRC <belenrob.alumn@cescristorey.com>
2024-02-28 22:01:35 +0100

    Pantalla de bienvenida

---
+++ PuntoInformacionCristoRey/PuntoInformacionCristoRey/Bienvenida.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace PuntoInformacionCristoRey
+{
+    partial class Bienvenida
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panelBienvenida = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panelBienvenida.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panelBienvenida
+            // 
+            this.panelBienvenida.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelBienvenida.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.panelBienvenida.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panelBienvenida.Controls.Add(this.pictureBox1);
+            this.panelBienvenida.Controls.Add(this.label2);
+            this.panelBienvenida.Controls.Add(this.label1);
+            this.panelBienvenida.Font = new System.Drawing.Font("Microsoft Sans Serif", 25.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.panelBienvenida.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(183)))), ((int)(((byte)(5)))));
+            this.panelBienvenida.Location = new System.Drawing.Point(12, 12);
+            this.panelBienvenida.Name = "panelBienvenida";
+            this.panelBienvenida.Size = new System.Drawing.Size(918, 469);
+            this.panelBienvenida.TabIndex = 0;
+            this.panelBienvenida.Click += new System.EventHandler(this.panelBienvenida_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.Image = global::PuntoInformacionCristoRey.Properties.Resources.Logo_Def_Fun_JGr_Hor__1_;
+            this.pictureBox1.Location = new System.Drawing.Point(124, 43);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(686, 238);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(89)))), ((int)(((byte)(168)))), ((int)(((byte)(217)))));
+            this.label2.Location = new System.Drawing.Point(118, 408);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(258, 32);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Pulse para acceder";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Left;
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
+            this.label1.Location = new System.Drawing.Point(115, 306);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(695, 102);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Bienvenido al punto de información\r\nCES Cristo Rey - Granada";
+            // 
+            // Bienvenida
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(942, 493);
+            this.Controls.Add(this.panelBienvenida);
+            this.Name = "Bienvenida";
+            this.Text = "PUNTO DE INFORMACIÓN CES CRISTO REY";
+            this.panelBienvenida.ResumeLayout(false);
+            this.panelBienvenida.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panelBienvenida;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PuntoInformacionCristoRey/PuntoInformacionCristoRey/Bienvenida.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuntoInformacionCristoRey
+{
+    public partial class Bienvenida : Form
+    {
+
+        PrivateFontCollection dosis = new PrivateFontCollection();
+        Font dosisLight;
+
+        //Paleta de colores
+        Color rosa = Color.FromArgb(242, 92, 132);
+        Color azulClaro = Color.FromArgb(89, 168, 217);
+        Color azulOscuro = Color.FromArgb(38, 111, 140);
+        Color amarillo = Color.FromArgb(242, 183, 5);
+        Color blanco = Color.FromArgb(242, 242, 242);
+
+        public Bienvenida()
+        {
+            InitializeComponent();
+
+            //Establecer el formulario en el centro de la pantalla
+            this.StartPosition = FormStartPosition.CenterScreen;
+
+            //Maximizar el formulario
+            this.WindowState = FormWindowState.Maximized;
+
+            //Maximizar el panel de bienvenida
+            panelBienvenida.Dock = DockStyle.Fill;
+
+            inicializarFuente();
+        }
+
+        private void inicializarFuente()
+        {
+            try
+            {
+                dosis.AddFontFile("dosis-light_[allfont.es].ttf");
+                dosisLight = new Font(dosis.Families[0], 26);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al cargar las fuentes: " + ex.Message);
+            }
+
+            //Establecer la fuente de los controles
+            foreach (Control c in this.Controls)
+            {
+                c.Font = dosisLight;
+            }
+
+            label1.Font = new Font(dosis.Families[0], 26);
+            label2.Font = new Font(dosis.Families[0], 20);
+
+        }
+
+        private void panelBienvenida_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
