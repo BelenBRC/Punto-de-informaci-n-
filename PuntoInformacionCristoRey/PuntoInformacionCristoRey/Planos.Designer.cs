c18191ea8f03cb88fb04f9425b2f47ec0102e48e
BelenBRC <belenrob.alumn@cescristorey.com>
2024-03-01 18:06:21 +0100

    Mapas de librerías cercanas

--- PuntoInformacionCristoRey/PuntoInformacionCristoRey/Planos.Designer.cs
+++ PuntoInformacionCristoRey/PuntoInformacionCristoRey/Planos.Designer.cs
@@ -30,17 +30,19 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Planos));
             this.splitContainer1 = new System.Windows.Forms.SplitContainer();
-            this.buttonPlanta1 = new System.Windows.Forms.Button();
-            this.buttonPlanta2 = new System.Windows.Forms.Button();
             this.buttonPlanta3 = new System.Windows.Forms.Button();
-            this.imagenPlano = new System.Windows.Forms.PictureBox();
+            this.buttonPlanta2 = new System.Windows.Forms.Button();
+            this.buttonPlanta1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.imagenPlano = new System.Windows.Forms.PictureBox();
+            this.buttonPapeleríasCercanas = new System.Windows.Forms.Button();
+            this.mapasGoogle = new GMap.NET.WindowsForms.GMapControl();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
             this.splitContainer1.Panel2.SuspendLayout();
             this.splitContainer1.SuspendLayout();
-            ((System.ComponentModel.ISupportInitialize)(this.imagenPlano)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenPlano)).BeginInit();
             this.SuspendLayout();
             // 
             // splitContainer1
@@ -52,6 +54,7 @@
             // splitContainer1.Panel1
             // 
             this.splitContainer1.Panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(183)))), ((int)(((byte)(5)))));
+            this.splitContainer1.Panel1.Controls.Add(this.buttonPapeleríasCercanas);
             this.splitContainer1.Panel1.Controls.Add(this.buttonPlanta3);
             this.splitContainer1.Panel1.Controls.Add(this.buttonPlanta2);
             this.splitContainer1.Panel1.Controls.Add(this.buttonPlanta1);
@@ -60,37 +63,10 @@
             // 
             this.splitContainer1.Panel2.Controls.Add(this.pictureBox1);
             this.splitContainer1.Panel2.Controls.Add(this.imagenPlano);
+            this.splitContainer1.Panel2.Controls.Add(this.mapasGoogle);
             this.splitContainer1.Size = new System.Drawing.Size(818, 528);
             this.splitContainer1.SplitterDistance = 187;
             this.splitContainer1.TabIndex = 0;
-            // 
-            // buttonPlanta1
-            // 
-            this.buttonPlanta1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonPlanta1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
-            this.buttonPlanta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPlanta1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.buttonPlanta1.Location = new System.Drawing.Point(0, 130);
-            this.buttonPlanta1.Name = "buttonPlanta1";
-            this.buttonPlanta1.Size = new System.Drawing.Size(186, 78);
-            this.buttonPlanta1.TabIndex = 0;
-            this.buttonPlanta1.Text = "1ª planta";
-            this.buttonPlanta1.UseVisualStyleBackColor = false;
-            this.buttonPlanta1.Click += new System.EventHandler(this.buttonPlanta1_Click);
-            // 
-            // buttonPlanta2
-            // 
-            this.buttonPlanta2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
-            this.buttonPlanta2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
-            this.buttonPlanta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonPlanta2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.buttonPlanta2.Location = new System.Drawing.Point(0, 214);
-            this.buttonPlanta2.Name = "buttonPlanta2";
-            this.buttonPlanta2.Size = new System.Drawing.Size(186, 78);
-            this.buttonPlanta2.TabIndex = 1;
-            this.buttonPlanta2.Text = "2ª planta";
-            this.buttonPlanta2.UseVisualStyleBackColor = false;
-            this.buttonPlanta2.Click += new System.EventHandler(this.buttonPlanta2_Click);
             // 
             // buttonPlanta3
             // 
@@ -98,7 +74,7 @@
             this.buttonPlanta3.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
             this.buttonPlanta3.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.buttonPlanta3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
-            this.buttonPlanta3.Location = new System.Drawing.Point(0, 298);
+            this.buttonPlanta3.Location = new System.Drawing.Point(0, 228);
             this.buttonPlanta3.Name = "buttonPlanta3";
             this.buttonPlanta3.Size = new System.Drawing.Size(186, 78);
             this.buttonPlanta3.TabIndex = 2;
@@ -106,17 +82,33 @@
             this.buttonPlanta3.UseVisualStyleBackColor = false;
             this.buttonPlanta3.Click += new System.EventHandler(this.buttonPlanta3_Click);
             // 
-            // imagenPlano
+            // buttonPlanta2
             // 
-            this.imagenPlano.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(183)))), ((int)(((byte)(5)))));
-            this.imagenPlano.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.imagenPlano.Image = global::PuntoInformacionCristoRey.Properties.Resources.planta1;
-            this.imagenPlano.Location = new System.Drawing.Point(0, 0);
-            this.imagenPlano.Name = "imagenPlano";
-            this.imagenPlano.Size = new System.Drawing.Size(627, 528);
-            this.imagenPlano.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.imagenPlano.TabIndex = 0;
-            this.imagenPlano.TabStop = false;
+            this.buttonPlanta2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlanta2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
+            this.buttonPlanta2.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlanta2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.buttonPlanta2.Location = new System.Drawing.Point(0, 144);
+            this.buttonPlanta2.Name = "buttonPlanta2";
+            this.buttonPlanta2.Size = new System.Drawing.Size(186, 78);
+            this.buttonPlanta2.TabIndex = 1;
+            this.buttonPlanta2.Text = "2ª planta";
+            this.buttonPlanta2.UseVisualStyleBackColor = false;
+            this.buttonPlanta2.Click += new System.EventHandler(this.buttonPlanta2_Click);
+            // 
+            // buttonPlanta1
+            // 
+            this.buttonPlanta1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPlanta1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
+            this.buttonPlanta1.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPlanta1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.buttonPlanta1.Location = new System.Drawing.Point(0, 60);
+            this.buttonPlanta1.Name = "buttonPlanta1";
+            this.buttonPlanta1.Size = new System.Drawing.Size(186, 78);
+            this.buttonPlanta1.TabIndex = 0;
+            this.buttonPlanta1.Text = "1ª planta";
+            this.buttonPlanta1.UseVisualStyleBackColor = false;
+            this.buttonPlanta1.Click += new System.EventHandler(this.buttonPlanta1_Click);
             // 
             // pictureBox1
             // 
@@ -130,6 +122,60 @@
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
             this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // imagenPlano
+            // 
+            this.imagenPlano.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(183)))), ((int)(((byte)(5)))));
+            this.imagenPlano.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.imagenPlano.Image = global::PuntoInformacionCristoRey.Properties.Resources.planta1;
+            this.imagenPlano.Location = new System.Drawing.Point(0, 0);
+            this.imagenPlano.Name = "imagenPlano";
+            this.imagenPlano.Size = new System.Drawing.Size(627, 528);
+            this.imagenPlano.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.imagenPlano.TabIndex = 0;
+            this.imagenPlano.TabStop = false;
+            // 
+            // buttonPapeleríasCercanas
+            // 
+            this.buttonPapeleríasCercanas.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Left | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonPapeleríasCercanas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(38)))), ((int)(((byte)(111)))), ((int)(((byte)(140)))));
+            this.buttonPapeleríasCercanas.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonPapeleríasCercanas.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(242)))), ((int)(((byte)(242)))), ((int)(((byte)(242)))));
+            this.buttonPapeleríasCercanas.Location = new System.Drawing.Point(0, 312);
+            this.buttonPapeleríasCercanas.Name = "buttonPapeleríasCercanas";
+            this.buttonPapeleríasCercanas.Size = new System.Drawing.Size(186, 78);
+            this.buttonPapeleríasCercanas.TabIndex = 3;
+            this.buttonPapeleríasCercanas.Text = "Papelerías cercanas";
+            this.buttonPapeleríasCercanas.UseVisualStyleBackColor = false;
+            this.buttonPapeleríasCercanas.Click += new System.EventHandler(this.buttonPapeleríasCercanas_Click);
+            // 
+            // mapasGoogle
+            // 
+            this.mapasGoogle.Bearing = 0F;
+            this.mapasGoogle.CanDragMap = true;
+            this.mapasGoogle.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mapasGoogle.EmptyTileColor = System.Drawing.Color.Navy;
+            this.mapasGoogle.GrayScaleMode = false;
+            this.mapasGoogle.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.mapasGoogle.LevelsKeepInMemmory = 5;
+            this.mapasGoogle.Location = new System.Drawing.Point(0, 0);
+            this.mapasGoogle.MarkersEnabled = true;
+            this.mapasGoogle.MaxZoom = 2;
+            this.mapasGoogle.MinZoom = 2;
+            this.mapasGoogle.MouseWheelZoomEnabled = true;
+            this.mapasGoogle.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.mapasGoogle.Name = "mapasGoogle";
+            this.mapasGoogle.NegativeMode = false;
+            this.mapasGoogle.PolygonsEnabled = true;
+            this.mapasGoogle.RetryLoadTile = 0;
+            this.mapasGoogle.RoutesEnabled = true;
+            this.mapasGoogle.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.mapasGoogle.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.mapasGoogle.ShowTileGridLines = false;
+            this.mapasGoogle.Size = new System.Drawing.Size(627, 528);
+            this.mapasGoogle.TabIndex = 2;
+            this.mapasGoogle.Visible = false;
+            this.mapasGoogle.Zoom = 0D;
             // 
             // Planos
             // 
@@ -145,8 +191,8 @@
             this.splitContainer1.Panel2.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
             this.splitContainer1.ResumeLayout(false);
-            ((System.ComponentModel.ISupportInitialize)(this.imagenPlano)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.imagenPlano)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -159,5 +205,7 @@
         private System.Windows.Forms.Button buttonPlanta1;
         private System.Windows.Forms.PictureBox imagenPlano;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonPapeleríasCercanas;
+        private GMap.NET.WindowsForms.GMapControl mapasGoogle;
     }
 }
--- PuntoInformacionCristoRey/PuntoInformacionCristoRey/Planos.cs
+++ PuntoInformacionCristoRey/PuntoInformacionCristoRey/Planos.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -16,6 +19,11 @@ namespace PuntoInformacionCristoRey
         PrivateFontCollection dosis = new PrivateFontCollection();
         Font dosisLight;
 
+        GMarkerGoogle marcador;
+        GMapOverlay capaMarcado;
+        DataTable dataTable;
+
+
         PanallaPrincipal principal;
         public Planos(PanallaPrincipal panalla)
         {
@@ -29,18 +37,21 @@ namespace PuntoInformacionCristoRey
 
         private void buttonPlanta1_Click(object sender, EventArgs e)
         {
+            mapasGoogle.Visible = false;
             //Colocar planta1 en el control de imagen
             imagenPlano.Image = Properties.Resources.planta1;
         }
 
         private void buttonPlanta2_Click(object sender, EventArgs e)
         {
+            mapasGoogle.Visible = false;
             //Colocar planta2 en el control de imagen
             imagenPlano.Image = Properties.Resources.planta2;
         }
 
         private void buttonPlanta3_Click(object sender, EventArgs e)
         {
+            mapasGoogle.Visible = false;
             //Colocar planta3 en el control de imagen
             imagenPlano.Image = Properties.Resources.planta3;
         }
@@ -64,12 +75,80 @@ namespace PuntoInformacionCristoRey
                 buttonPlanta1.Font = dosisLight;
                 buttonPlanta2.Font = dosisLight;
                 buttonPlanta3.Font = dosisLight;
+                buttonPapeleríasCercanas.Font = dosisLight;
             }
             catch (Exception ex)
             {
                 MessageBox.Show("Error al cargar las fuentes: " + ex.Message);
             }
 
+        }
+
+        private void buttonPapeleríasCercanas_Click(object sender, EventArgs e)
+        {
+            //Mostrar el componente mapasGoogle
+            mapasGoogle.Visible = true;
+            //Traer al frente
+            mapasGoogle.BringToFront();
+            pictureBox1.BringToFront();
+
+            cargarMapa();
+            marcadoresLibrerias();
+        }
+
+        private void cargarMapa()
+        {
+            double latitud = 37.18426;
+            double longitud = -3.59338;
+
+            mapasGoogle.DragButton = MouseButtons.Left;
+            mapasGoogle.CanDragMap = true;
+            mapasGoogle.MapProvider = GMapProviders.GoogleMap;
+            mapasGoogle.Position = new GMap.NET.PointLatLng(latitud, longitud);
+            mapasGoogle.MinZoom = 0;
+            mapasGoogle.MaxZoom = 22;
+            mapasGoogle.Zoom = 18;
+            mapasGoogle.AutoScroll = true;
+        }
+
+        private void marcadoresLibrerias()
+        {
+            double latitud = 0;
+            double longitud = 0;
+
+            capaMarcado = new GMapOverlay();
+
+            //Marcador colegio
+            latitud = 37.18426;
+            longitud = -3.59338;
+            marcador = new GMarkerGoogle(new GMap.NET.PointLatLng(latitud, longitud), GMarkerGoogleType.red_pushpin);
+            capaMarcado.Markers.Add(marcador);
+            marcador.ToolTipMode = MarkerTooltipMode.Always;
+            marcador.ToolTipText = "CES Cristo Rey";
+
+            //Marcadores papelerías
+            latitud = 37.18410;
+            longitud = -3.59371;
+            marcador = new GMarkerGoogle(new GMap.NET.PointLatLng(latitud, longitud), GMarkerGoogleType.green);
+            capaMarcado.Markers.Add(marcador);
+            marcador.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marcador.ToolTipText = "Librería Papelería Albayzín";
+
+            latitud = 37.18263;
+            longitud = -3.59336;
+            marcador = new GMarkerGoogle(new GMap.NET.PointLatLng(latitud, longitud), GMarkerGoogleType.green);
+            capaMarcado.Markers.Add(marcador);
+            marcador.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marcador.ToolTipText = "Papelería Plaza Larga";
+
+            latitud = 37.18312;
+            longitud = -3.59327;
+            marcador = new GMarkerGoogle(new GMap.NET.PointLatLng(latitud, longitud), GMarkerGoogleType.green);
+            capaMarcado.Markers.Add(marcador);
+            marcador.ToolTipMode = MarkerTooltipMode.OnMouseOver;
+            marcador.ToolTipText = "Expendeduría Albayzín N19";
+
+            mapasGoogle.Overlays.Add(capaMarcado);
         }
     }
 }
